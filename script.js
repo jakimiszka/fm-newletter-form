8ac5aecbf41e447f551e6afd1598cc9a290cea9b
jakimiszka <jarek.jakimicha@gmail.com>
2025-08-12 21:18:27 +0200

    final?

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const form = document.querySelector("form");
 const errrorMessage = document.querySelector(".error_message");
-const emailInput = document.querySelector(".email");
+const emailInput = document.querySelector(".email_input");
+const card = document.querySelector(".container");
+const modal = document.querySelector(".modal");
+const modal_button = document.querySelector(".modal_button");
 
 function validateEmail(email) {
   const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -9,20 +12,42 @@ function validateEmail(email) {
 
 function handleSubmit(e) {
   e.preventDefault();
-  const data = {};
-  const fields = e.target.querySelectorAll("input");
-  for (const field of fields) {
-    data[field.name] = field.value;
-  }
-  if (!validateEmail(data.email)) {
-    errrorMessage.style.display = "block"; // toggle with data-attr?
-    emailInput.value = "";
-    return;
-  } else {
-    errrorMessage.style.display = "none";
-    alert("Newsletter subscription successful!");
-    return;
+  const email = emailInput.value;
+  if (!validateEmail(email) === false) {
+    card.style.display = "none";
+    modal.style.display = "flex";
   }
 }
 
 form.addEventListener("submit", handleSubmit);
+
+function validateInput() {
+  const data = emailInput.value;1
+  if (!validateEmail(data)) {
+    emailInput.classList.add('error_input');
+    emailInput.classList.add('invalid_placeholder');
+    errrorMessage.style.display = "block";
+  }else{
+    emailInput.classList.remove('error_input');
+    emailInput.classList.remove('invalid_placeholder');
+    errrorMessage.style.display = "none";
+  }
+}
+
+modal_button.addEventListener("click", () => {
+    card.style.display = "grid";
+    modal.style.display = "none";
+});
+
+document.addEventListener("focus", (e) => {
+  if (e.target === emailInput) {
+    validateInput();
+    errrorMessage.style.display = "none";
+  }
+}, true);
+
+document.addEventListener("blur", (e) => {
+  if (e.target === emailInput) {
+    validateInput();
+  }
+}, true);
